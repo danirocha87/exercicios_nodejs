7d84aef0be244944218a454d1c14922280193690
Dani Rocha <danioliver_17@hotmail.com>
2020-06-19 20:40:32 -0300

    atualizar o texto

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,5 +1,9 @@
-// ## Importe a biblioteca ##
+let rs = require("readline-sync");
 
+let numvoo = rs.question("Qual e seu voo?")
+let fileira = rs.question("Em qual fileira deseja sentar?")
+console.log("Cartao de embarque para o voo de numero:" +numvoo)
+ console.log("=========================================")
 
-
-// ## Faça o código ##
+console.log("Voce sentara na fileira:" +fileira)
+console.log("============================")
