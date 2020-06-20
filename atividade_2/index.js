5a47b088b8611e430761e58797b1826d010890cf
Dani Rocha <danioliver_17@hotmail.com>
2020-06-19 22:21:13 -0300

    atividade 2

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,5 +1,13 @@
-// ## Importe a biblioteca ##
+let rs = require("readline-sync");
+
+let mascara  = rs.question("Voce esta usando mascara quando precisa sair de casa ( sim ou nao) ?")
+
+let maos = rs.question(" Voce esta lavando as maos frequentemente ? ")
 
 
 
-// ## Faça o código ##
+if( mascara === "sim" && maos === "sim"){
+    console. log("Parabens com atitudes assim iremos vencer o covid19")
+} else {
+    console. log("Poxa que triste assim voce nao esta ajudando a combater o covid19,por favor se cuide.")
+}
